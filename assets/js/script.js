a780240e2a6aaa33adcb3de187b1c85360abf1bf
Sergii Kostanets <sergiikostanets@gmail.com>
2023-01-19 15:45:18 +0000

    add computer move

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const choice = ["rock", "paper", "scissors", "lizard", "spock"]
+
 document.addEventListener("DOMContentLoaded", function () {
 
     let buttons = document.getElementsByTagName("button")
@@ -9,13 +11,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
             runGame(weaponType)
 
+            computerMove()
+
+
+
         })
     }
 
 })
 
 function runGame(weaponType) {
+
     let playerChoice = document.getElementById("player-choice")
     playerChoice.setAttribute("src", `assets/images/${weaponType}.jpeg`)
+
 }
 
+function computerMove() {
+
+    let ai = Math.floor(Math.random() * 5)
+    ai = choice[ai]
+    let computerChoice = document.getElementById("computer-choice")
+    computerChoice.setAttribute("src", `assets/images/${ai}.jpeg`)
+
+}
